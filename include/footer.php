0bd9e691062754d733f2f8a523f2950be7047b83
Mamun0796 <abdulalmamun637@gmail.com>
2024-06-08 12:42:02 +0600

    First commit

---
+++ include/footer.php
@@ -0,0 +1,54 @@
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="<?= $baseurl ?>asset/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="<?= $baseurl ?>asset/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?= $baseurl ?>asset/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="<?= $baseurl ?>asset/vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="<?= $baseurl ?>asset/vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="<?= $baseurl ?>asset/vendors/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="<?= $baseurl ?>asset/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="<?= $baseurl ?>asset/vendors/iCheck/icheck.min.js"></script>
+    <!-- Skycons -->
+    <script src="<?= $baseurl ?>asset/vendors/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="<?= $baseurl ?>asset/vendors/Flot/jquery.flot.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/Flot/jquery.flot.time.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="<?= $baseurl ?>asset/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="<?= $baseurl ?>asset/vendors/DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="<?= $baseurl ?>asset/vendors/jqvmap/dist/jquery.vmap.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="<?= $baseurl ?>asset/vendors/moment/min/moment.min.js"></script>
+    <script src="<?= $baseurl ?>asset/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="<?= $baseurl ?>asset/build/js/custom.min.js"></script>
+	
+  </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,53 @@
+<?php
+    include_once('class/crud.php');
+    session_start();
+    if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']){
+
+    }else{
+        header('location:login.php');
+    }
+?>
+
+<?php $baseurl="http://localhost/eticket/"; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="<?= $baseurl ?>asset/images/favicon.ico" type="image/ico" />
+
+    <title>Gentelella Alela!</title>
+
+    <!-- Bootstrap -->
+    <link href="<?= $baseurl ?>asset/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?= $baseurl ?>asset/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?= $baseurl ?>asset/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="<?= $baseurl ?>asset/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+	
+    <!-- bootstrap-progressbar -->
+    <link href="<?= $baseurl ?>asset/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="<?= $baseurl ?>asset/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <!-- bootstrap-daterangepicker -->
+    <link href="<?= $baseurl ?>asset/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="<?= $baseurl ?>asset/build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+            </div>
+
+            <div class="clearfix"></div>
