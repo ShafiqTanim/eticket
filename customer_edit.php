dd9c68ccf2d3c38e1f81cb6e816506fe80023c6a
ShafiqTanim <shafiqullah715@gmail.com>
2024-06-19 21:55:03 +0600

    customer crud fixed

--- customer_edit.php
+++ customer_edit.php
@@ -1,9 +1,6 @@
-<?php include('include/header.php') ; ?>
+<?php include_once('include/header.php') ?>
+<?php include_once('include/sidebar.php') ?>
 
-<!-- Content -->
-
-<div class="container-xxl flex-grow-1 container-p-y">
-    <h4 class="py-3 mb-4"><span class="text-muted fw-light">Customer/</span> Update</h4>
     <?php 
         $olddata=array();
         $con['id']=$_GET['id'];
@@ -14,49 +11,96 @@
             }
         }
     ?>
-    <!-- Basic Layout -->
-    <div class="row">
-    <div class="col-xl">
-        <div class="card mb-4">
-        <div class="card-header d-flex justify-content-between align-items-center">
-            <h5 class="mb-0">Customer Information</h5>
-        </div>
-        <div class="card-body">
-            <form method="post" action="">
-                <div class="mb-3">
-                    <label class="form-label" for="fullname">Full Name</label>
-                    <input type="text" name="name" class="form-control" id="fullname" value="<?= $olddata->name ?>" />
+    <!-- page content -->
+    <div class="right_col" role="main">
+        <div class="">
+            <div class="page-title">
+                <div class="title_left">
+                    <h3>Customer</h3>
                 </div>
-                <div class="mb-3">
-                    <label class="form-label" for="phone">Phone No</label>
-                    <input type="text" name="contact_no" id="phone" class="form-control phone-mask" value="<?= $olddata->contact ?>" />
-                </div>
-                <div class="mb-3">
-                    <label class="form-label" for="due">Email</label>
-                    <input type="text" name="email" id="email" class="form-control" value="<?= $olddata->due ?>" />
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            <?php 
-                if($_POST){
-                    $_POST['udated_at']=date('Y-m-d H:i:s');
-                    $_POST['updated_by']=1;
-                    $rs=$mysqli->common_update('customer',$_POST,$con);
-                    if($rs){
-                        if($rs['data']){
-                            echo "<script>window.location='{$baseurl}customer_list.php'</script>";
-                        }else{
-                            echo $rs['error'];
-                        }
-                    }
-                }
-            ?>
-        </div>
-        </div>
-    </div>
-    
-    </div>
-</div>
-<!-- / Content -->
 
-<?php include('include/footer.php') ; ?>
+                <div class="title_right">
+                    <div class="col-md-5 col-sm-5 form-group pull-right top_search">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">Go!</button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Edit</h2>
+                            <ul class="nav navbar-right panel_toolbox">
+                                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">Settings 1</a>
+                                        <a class="dropdown-item" href="#">Settings 2</a>
+                                    </div>
+                                </li>
+                                <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                </li>
+                            </ul>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="x_content">
+                            <form class="" action="" method="post" novalidate>
+                                <div class="field item form-group">
+                                    <label class="col-form-label col-md-3 col-sm-3  label-align">Name<span class="required">*</span></label>
+                                    <div class="col-md-6 col-sm-6">
+                                        <input class="form-control" class='optional' name="name" value="<?= $olddata->name ?>" data-validate-length-range="5,15" type="text" /></div>
+                                </div>
+                                <div class="field item form-group">
+                                    <label class="col-form-label col-md-3 col-sm-3  label-align">Contact No<span class="required">*</span></label>
+                                    <div class="col-md-6 col-sm-6">
+                                        <input class="form-control" data-validate-length-range="6" data-validate-words="2" value="<?= $olddata->contact_no ?>" name="contact_no" required="required" />
+                                    </div>
+                                </div>
+                                <div class="field item form-group">
+                                    <label class="col-form-label col-md-3 col-sm-3  label-align">Email<span class="required">*</span></label>
+                                    <div class="col-md-6 col-sm-6">
+                                        <input class="form-control" data-validate-length-range="6" data-validate-words="2" value="<?= $olddata->email ?>" name="email" required="required" />
+                                    </div>
+                                </div>
+                                <div class="ln_solid">
+                                    <div class="form-group">
+                                        <div class="col-md-6 offset-md-3">
+                                            <button type='submit' class="btn btn-primary">Submit</button>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <?php 
+                                if($_POST){
+                                    $_POST['updated_at']=date('Y-m-d H:i:s');
+                                    $_POST['updated_by']=1;
+                                    $rs=$mysqli->common_update('customer',$_POST, $con);
+                                    if($rs){
+                                        if($rs['data']){
+                                            echo "<script>window.location='{$baseurl}customer_list.php'</script>";
+                                        }else{
+                                            echo $rs['error'];
+                                        }
+                                    }
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /page content -->
+
+    <!-- /page footer -->
+    <?php include('include/footer.php') ?>
