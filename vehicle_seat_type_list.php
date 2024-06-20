dab42fe592461f8f55c969b46ddd875944e9e414
AhmedMukut <ahmedmukut19@gmail.com>
2024-06-20 11:29:03 +0600

    area,counter,route,schedule,seat,seat_type,vehicle_seat_type added

---
+++ vehicle_seat_type_list.php
@@ -0,0 +1,94 @@
+<?php include('include/header.php') ?>
+<?php include('include/sidebar.php') ?>
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Vehicle</h3>
+              </div>
+
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                      <button class="btn btn-secondary" type="button">Go!</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>List</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Settings 1</a>
+                            <a class="dropdown-item" href="#">Settings 2</a>
+                          </div>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+
+                    <!-- start project list -->
+                    <table class="table table-striped projects">
+                      <thead>
+                        <tr>
+                          <th style="width: 1%">#</th>
+                          <th style="width: 20%">Vehicle id</th>
+                          <th style="width: 20%">Seat id</th>
+                          <th style="width: 20%">Seat Type id</th>
+                          <th style="width: 20%">Price</th>
+                          <th style="width: 20%">Active</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                          <?php 
+                            $result=$mysqli->common_select('Vehicle_Seat_type ');
+                            if($result){
+                                if($result['data']){
+                                    $i=1;
+                                    foreach($result['data'] as $data){
+                          ?>
+                            <tr>
+                                <td><?= $i++ ?></td>
+                                <td><?= $data-> vehicle_id ?></td>
+                                <td><?= $data-> seat_id ?></td>
+                                <td><?= $data-> seat_type_id ?></td>
+                                <td><?= $data-> price ?></td>
+                                <td>
+                                  <a href="<?= $baseurl ?>Vehicle_Seat_type _edit.php?id=<?= $data ->id ?>" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit </a>
+                                  <a href="<?= $baseurl ?>Vehicle_Seat_type _delete.php?id=<?= $data ->id ?>" class="btn btn-Warning btn-xs"><i class="fa fa-trash-o"></i> Delete </a>
+                                </td>
+                                
+                            </tr>
+                          <?php } } } ?>
+                      </tbody>
+                    </table>
+                    <!-- end project list -->
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <?php include('include/footer.php') ; ?>
